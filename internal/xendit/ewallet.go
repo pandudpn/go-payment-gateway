333c8c10970ecd9e7925044315a79e9bf77b33e7
Pandu dwi Putra <pandu@pandudpn.id>
2022-07-23 02:11:04 +0700

    update return of CreateRequest EWallet Xendit

--- internal/xendit/ewallet.go
+++ internal/xendit/ewallet.go
@@ -6,6 +6,6 @@ import (
 
 func (e *EWallet) CreateRequest() PaymentInterface {
 	payload, _ := json.Marshal(e)
-	
+
 	return &request{params: payload}
 }
--- internal/xendit/xendit.go
+++ internal/xendit/xendit.go
@@ -8,10 +8,10 @@ import (
 type PaymentInterface interface {
 	// SetUsername will set username in Basic Auth
 	SetUsername(username string)
-	
+
 	// SetURI set url target
 	SetURI(uri string)
-	
+
 	// Do create a charge payments
 	Do(ctx context.Context) (*ChargeResponse, error)
 }
