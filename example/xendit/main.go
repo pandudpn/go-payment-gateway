d571d72e2839c8e6db0e908e1b7aac4b714ffc2c
Pandu dwi Putra <pandu@pandudpn.id>
2022-07-29 23:15:57 +0700

    remove log options in example

--- example/xendit/main.go
+++ example/xendit/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-
+	
 	"github.com/google/uuid"
 	"github.com/pandudpn/go-payment-gateway"
 	"github.com/pandudpn/go-payment-gateway/internal/xendit"
@@ -11,12 +11,12 @@ import (
 func main() {
 	var err error
 	payment := pg.New()
-
+	
 	err = payment.SetXenditCredentials("xnd_development_spnIw1pkbX4akj5wfZ9DBAmYIRFlikR1fmiROpTk51IWgek4JwfW8YcKSJ1rUMU")
 	if err != nil {
 		panic(err)
 	}
-
+	
 	xndEWallet := &xendit.EWallet{
 		ChannelCode: xendit.EWalletOVO,
 		Amount:      10000,
@@ -26,16 +26,16 @@ func main() {
 			SuccessRedirectURL: "https://www.google.com",
 		},
 	}
-
+	
 	xnd, err := payment.Xendit()
 	if err != nil {
 		panic(err)
 	}
-
+	
 	charge, err := xnd.CreateEWalletCharge(xndEWallet)
 	if err != nil {
 		panic(err)
 	}
-
+	
 	fmt.Println("charge", *charge)
 }
