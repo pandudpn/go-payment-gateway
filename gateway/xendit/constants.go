41a677e0be1747eca69fc05a73c3fe27f8295a3f
Pandu dwi Putra <pandu@pandudpn.id>
2022-08-14 01:15:11 +0700

    add bank code and status va

--- gateway/xendit/constants.go
+++ gateway/xendit/constants.go
@@ -9,6 +9,9 @@ const (
 
 	// PHP is Philippine Peso
 	PHP Currency = "PHP"
+
+	// USD is American Dollar
+	USD Currency = "USD"
 )
 
 // ItemType type of item or product in basket
@@ -94,4 +97,53 @@ const (
 
 	// Refunded payment transaction for specified charge_id has been either partially or fully refunded
 	Refunded ChargeStatus = "REFUNDED"
+)
+
+// BankCode for the relevant bank
+type BankCode string
+
+const (
+	// BankBCA is code bank for payment virtual account using BCA
+	BankBCA BankCode = "BCA"
+
+	// BankBNI is code bank for payment virtual account using BNI
+	BankBNI BankCode = "BNI"
+
+	// BankBRI is code bank for payment virtual account using BRI
+	BankBRI BankCode = "BRI"
+
+	// BankBJB is code bank for payment virtual account using BJB
+	BankBJB BankCode = "BJB"
+
+	// BankBSI is code bank for payment virtual account using BSI
+	BankBSI BankCode = "BSI"
+
+	// BankCIMB is code bank for payment virtual account using CIMB
+	BankCIMB BankCode = "CIMB"
+
+	// BankDBS is code bank for payment virtual account using DBS
+	BankDBS BankCode = "DBS"
+
+	// BankMandiri is code bank for payment virtual account using Mandiri
+	BankMandiri BankCode = "MANDIRI"
+
+	// BankPermata is code bank for payment virtual account using Permata
+	BankPermata BankCode = "PERMATA"
+
+	// BankSahabatSampoerna is code bank for payment virtual account using Sahabat Sampoerna
+	BankSahabatSampoerna BankCode = "SAHABAT_SAMPOERNA"
+)
+
+// StatusVA status of virtual account
+type StatusVA string
+
+const (
+	// PendingVA is when VA creation request has been sent and request is being processed by the bank
+	PendingVA StatusVA = "PENDING"
+
+	// ActiveVA the virtual account ready to be used by customer
+	ActiveVA StatusVA = "ACTIVE"
+
+	// InactiveVA either the single use VA has been paid or already expired
+	InactiveVA StatusVA = "INACTIVE"
 )
