0cf7f6bedbef479ee9992ebc100641cf4febcbe2
Pandu dwi Putra <pandu@pandudpn.id>
2022-08-14 22:00:04 +0700

    add function for updating virtual account

--- gateway/xendit/va.go
+++ gateway/xendit/va.go
@@ -51,3 +51,46 @@ func (x *xendit) createVirtualAccount(ctx context.Context) (*VirtualAccount, err
 
 	return &va, nil
 }
+
+// UpdateVirtualAccount will update an existing data Virtual Account Number
+func UpdateVirtualAccount(uva *UpdateVirtualAccountParam, opts *pg.Options) (*VirtualAccount, error) {
+	x, err := createChargeXendit(uva, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(10)*time.Second)
+	defer cancel()
+
+	return x.updateVirtualAccount(ctx)
+}
+
+// UpdateVirtualAccountWithContext will update an existing data Virtual Account Number with context params
+func UpdateVirtualAccountWithContext(ctx context.Context, uva *UpdateVirtualAccountParam, opts *pg.Options) (*VirtualAccount, error) {
+	x, err := createChargeXendit(uva, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return x.updateVirtualAccount(ctx)
+}
+
+func (x *xendit) updateVirtualAccount(ctx context.Context) (*VirtualAccount, error) {
+	var (
+		va     VirtualAccount
+		err    error
+		header = make(http.Header)
+	)
+	// set uri target
+	x.uri = uri + vaUri + x.pathParams
+
+	// set basic auth
+	header.Set("Authorization", utils.SetBasicAuthorization(x.opts.ServerKey, ""))
+
+	err = x.opts.ApiCall.Call(ctx, http.MethodPatch, x.uri, header, x.params, &va)
+	if err != nil {
+		return nil, err
+	}
+
+	return &va, nil
+}
